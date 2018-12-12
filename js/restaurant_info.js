c1f0ccc5f46de05bf3a3d5ce713e9eb5b06e4051
szeidman <sam.zeidman@gmail.com>
2018-12-11 20:09:44 -0500

    style: Begin implementing flexbox for restaurants-list class and related objects

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -151,7 +151,7 @@ createReviewHTML = (review) => {
   const name = document.createElement('p');
   name.innerHTML = review.name;
   li.appendChild(name);
-
+//   element.classList.add("mystyle");
   const date = document.createElement('p');
   date.innerHTML = review.date;
   li.appendChild(date);

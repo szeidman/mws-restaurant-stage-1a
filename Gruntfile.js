5ac5b9d3c1052aef7360ce5a29c0ab32a64a9c4f
szeidman <sam.zeidman@gmail.com>
2018-12-16 15:14:44 -0500

    chore: Reconfigure Gruntfile and generate images of different sizes

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 800,
+            suffix: '_large',
+            quality: 50
+          },
+          {
+            width: 400,
+            suffix: '_small',
+            quality: 50
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'img/',
+          dest: 'images/'
+        }]
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
+
+};

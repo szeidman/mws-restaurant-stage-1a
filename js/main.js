95c0ecd091688588bf58372cbd3959042186d4fe
szeidman <sam.zeidman@gmail.com>
2018-12-20 16:18:50 -0500

    feature: Add and style skip link

--- js/main.js
+++ js/main.js
@@ -49,6 +49,7 @@ fillNeighborhoodsHTML = (neighborhoods = self.neighborhoods) => {
     const option = document.createElement('option');
     option.innerHTML = neighborhood;
     option.value = neighborhood;
+    option.label = neighborhood;
     select.append(option);
   });
 }
